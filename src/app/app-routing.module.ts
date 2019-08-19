0b88bd7e24cafd218eeb5d60be44ac509df83b97
ThlD <madev.selous@gmail.com>
2019-08-19 07:47:56 +0300

    Routing config updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules
+    preloadingStrategy: PreloadAllModules,
+    useHash: true
   })],
   exports: [RouterModule],
   declarations: [],
